8dce988340f1ca2e1ad804aa5a41d2edb732e602
Joude1411 <jonathan.ude@nhs.net>
2019-12-05 22:40:48 +0000

    cleaning up Data_Wrangling.R

--- Data_Wrangling.R
+++ Data_Wrangling.R
@@ -5,7 +5,7 @@
 library(odbc)
 #library(DBI)
 library(lubridate)
-library(tidyverse) # tidyverse contains library(readr),library(readxl),library(lubridate),library(stringr),#library(dplyr),library(tidyr)
+library(tidyverse) # tidyverse contains library(readr),library(readxl),library(stringr),#library(dplyr),library(tidyr)
                     # and library("ggplot2", lib.loc= "C:/Users/ju0d/Documents/R/win-library/3.6") #library(gglot2) stopped working after upgrade to version 3.6.1
 
 
@@ -56,7 +56,7 @@ shinyData <- myData%>%
          ,Business_Unit)%>%
   mutate(Performance = ifelse(Denominator <= 0|Denominator==''|is.null(Denominator)|is.na(Denominator)
                                ,Numerator,Numerator/Denominator))%>%
-  select(Financial_Year,Month,Report_Date,Indicator_Code,Indicator_Name,Numerator,Denominator,Performance#,Site_Code
+  select(Financial_Year,Month,Report_Date,Indicator_Code,Indicator_Name,Numerator,Denominator,Performance
          ,Business_Unit)
 
 
@@ -66,9 +66,6 @@ sFile <- "shinyData_New_Table.csv"
 write_csv(shinyData,file.path(sFldr,sFile)) #write.csv(shinyData,[path]) will also work
 
 # End of Data wrangling
-
-
-
 
 
 #To export the file as excel instead of csv, use
@@ -85,64 +82,5 @@ write.xlsx(shinyData,file.path(sFldr,sFile))
 new_ShinyData <- read_csv(path) #for the csv files
 new_ShinyData2 <- read_xlsx(file.path(sFldr,sFile)) #for excel files
 
-head(new_ShinyData2)
 
 
-RunData <- new_ShinyData2%>%
-  filter(Indicator_Code %in% c('LT036a'))%>%
-  arrange(ReportDate)%>%
-  select(y=Performance,date=ReportDate,grp=Metric_name)%>%
-  filter(date >= '2015-04-01')%>%
-  runcharter(
-    med_rows = 12,
-    runlength = 8,
-    chart_title = "Analysis of RFL Performance",
-    chart_subtitle = "Performance Report",
-    direction = "both",#direction can be "below", "above" or "both"
-    facet_cols = 1
-    #line_colr = "#005EB8",    # blue
-    #point_colr ="#005EB8",    # blue
-    # median_colr = "#E87722",  # orange
-    # sus_fill = "#DB1884",     # magenta
-  )
-RunData 
-
-# end of runcharter script
-
-View(RunData$sustained)
-
-
-# utilizing the output of the run (i.e sustained)
-
-RunData$sustained%>%
-  group_by(grp,new_median,run_start-run_end)%>%
-  summarize(shift=max(month(run_start)))
-
-
-RunData$sustained%>%
-  group_by(grp,new_median,run_start,run_end)%>%
-  mutate(differ = run_start - run_end)%>%
-  select(differ)
-summarize(shift=max(month(run_start)))
-
-max(RunData$sustained[,5])
-min(RunData$sustained[,5])
-nrow(RunData$sustained)
-
-total_rec <- nrow(RunData$sustained)
-RunData$sustained%>%
-  select(new_median,run_start,run_end)%>%
-  mutate(total_rec)%>%
-  filter(run_start[3])
-
-
-RunData$sustained[num,6]
-RunData$sustained[num-1,6]
-num<-nrow(RunData$sustained)
-test <- ifelse(RunData$sustained[num,6]>RunData$sustained[num-1,6],"Outlier","Great")
-test  
-
-
-rm(zeros)
-
-View(myData)
